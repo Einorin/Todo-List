3103285a61ca745ad55c18cb1ec7ae8db61ae673
Ronnie Bunao <ronniebunao001@gmail.com>
2023-07-22 12:45:41 +0800

    Add UI design mainly using css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import './style.css'
-import printMe from './print';
-
-const test = document.querySelector('.test')
-const h1 = document.querySelector('h1');
-const btn = document.createElement('button');
-btn.innerHTML = 'Click me'
-btn.onclick = printMe;
-test.appendChild(btn)
 
